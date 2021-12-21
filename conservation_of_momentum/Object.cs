34fb475c5036c8f15b749bd2cf730f05583cf51a
Anklat <92557292+Anklat@users.noreply.github.com>
2021-12-21 14:44:41 -0500

    Update Object.cs

--- conservation_of_momentum/Object.cs
+++ conservation_of_momentum/Object.cs
@@ -17,13 +17,10 @@ namespace conservation_of_momentum
         public double calculate_Vf(double Vi2, double Vf2, double m2)
         {
             // Calculate final object velocity
-            #region NotImportantThings
-
             double Pi = m * Vi + m2 * Vi2;
             double d = m2 * Vf2;
             Pi = Pi - d;
             Vf = Pi / m;
-            #endregion
 
             return Vf;
         }
